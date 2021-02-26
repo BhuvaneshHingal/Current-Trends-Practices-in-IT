d02df071e50bf18a9b73347430dd63f1b72dad00
Bhuvanesh Hingal <58567847+BhuvaneshHingal@users.noreply.github.com>
2021-02-26 11:59:29 +0530

    Create ArrayFunctions.ts

---
+++ TypeScript/ArrayFunctions.ts
@@ -0,0 +1,28 @@
+/* Array Functions.
+1. concat: returns a new array after one array is contenated with the second array.
+2. join: Joins all the elements of an Array into a String.
+3. pop: pop deletes the last element from an array.
+4. push: push adds the element at the last position.
+5. reverse: reverse method reverses the array.
+6. shift: shift method deletes the first element from the array.
+7. sort: sorts the array lexicographically.
+8. toString: converts the array to string.
+9. unshift: adds an element at the start / beginning of the array.
+
+- every(): input as a function name
+function name: do something for every element of the array.
+returns true if the condition defined in the function is true for all the elements. return false if the condition fails for even a single element in the array
+- filter()
+-forEach()
+var arr = [10,20,30,40]
+arr.forEach( function (value)
+{
+    if(value<=10)
+        {
+            console.log(value)
+        }
+}
+)
+
+- indexOf()
+*/
