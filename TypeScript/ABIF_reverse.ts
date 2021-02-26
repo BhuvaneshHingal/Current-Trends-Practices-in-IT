e7f92d952e61094740070998753976a739a21a3f
Bhuvanesh Hingal <58567847+BhuvaneshHingal@users.noreply.github.com>
2021-02-26 12:04:54 +0530

    Create ABIF_reverse.ts

---
+++ TypeScript/ABIF_reverse.ts
@@ -0,0 +1,5 @@
+/* Accept an array of strings and print it in reverse order. */
+console.clear()
+var str = newArray("Apple","Banana","Watermelon")
+var reversed = str.reverse() 
+console.log(reversed)
