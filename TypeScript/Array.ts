863823b5a6f8cc6d7bbec97ebb56c86f83ada76c
Bhuvanesh Hingal <58567847+BhuvaneshHingal@users.noreply.github.com>
2021-02-26 11:13:49 +0530

    Add Array.ts
    Program to accept an array and display it

---
+++ TypeScript/Array.ts
@@ -0,0 +1,10 @@
+//Write a TS program to accept an array and display it.
+var odds = []
+for(var i=0; i<5; i++)
+{
+  odds[i] = Number (prompt("Enter a number: "))
+}
+for(var i=0; i<odds.length; i++)
+{
+  console.log(odds[i])
+}
